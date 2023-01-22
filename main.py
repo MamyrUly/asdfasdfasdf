38d852bdd270593a661db782415c7fd9964cfcd7
MamyrUly <117902209+MamyrUly@users.noreply.github.com>
2023-01-22 13:55:26 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    pass
+
+    # ans = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
+
+x = [[i] for i in range(10)]
+print(x)
+
+u = [[]]
+for i in range(8):
+    u[u.append(i)]
+
+print(u)
